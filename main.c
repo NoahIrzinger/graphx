5ee6e201dd9f4a39703391b949d3c45b832940fc
Noah Irzinger <noah.irzinger@gmail.com>
2024-04-10 23:52:24 -0700

    refactor plotting logic to handle all quadrants

--- main.c
+++ main.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 static unsigned int PIXEL_LENGTH = 12;
-
-static unsigned int GRAPH_PIXELS_DEFAULT_WIDTH = 300;
-static unsigned int GRAPH_PIXELS_DEFAULT_HEIGHT = 300;
+static unsigned int GRAPH_SIZE_SCALE = 10;
 static unsigned int GRAPH_TICK_PIXELS_DEFAULT_INTERVAL = 10;
 
 struct Point
 {
     int data_x_pos;
     int data_y_pos;
-    int graph_x_pos;
-    int graph_y_pos;
+    unsigned int graph_x_pos;
+    unsigned int graph_y_pos;
     size_t value;
 };
 
-void write_raster_to_ppm(char raster_data[], char output_file_path[], int graph_pixels_width, int graph_pixels_height)
+void write_raster_to_ppm(char raster_data[], char output_file_path[], unsigned int graph_pixels_width, unsigned int graph_pixels_height)
 {
 
     char MAGIC_NUMBER[] = "P3";
@@ -35,7 +34,7 @@ void write_raster_to_ppm(char raster_data[], char output_file_path[], int graph_
     fclose(f);
 }
 
-void paint_raster(char *raster, int raster_row_length, int hex_values[], int graph_pixels_width, int graph_pixels_height)
+void paint_raster(char *raster, unsigned int raster_row_length, int hex_values[], unsigned int graph_pixels_width, unsigned int graph_pixels_height)
 {
     for (int i = 0; i < graph_pixels_height; i++)
     {
@@ -62,12 +61,15 @@ void paint_raster(char *raster, int raster_row_length, int hex_values[], int gra
     // printf("%s\n", raster);
 }
 
-void generate_hexcodes(int *hexcodes, int raster_row_length, int graph_pixels_width, int graph_pixels_height, int graph_tick_pixels_interval, struct Point points[], int points_length)
+void generate_hexcodes(int *hexcodes, unsigned int raster_row_length, unsigned int graph_pixels_width, unsigned int graph_pixels_height, struct Point points[], unsigned int points_length)
 {
     int PLOT_Y_AXIS_OFFSET = 5;
     int PLOT_Y_AXIS_WIDTH = 1;
     int PLOT_X_AXIS_OFFSET = 5;
     int PLOT_X_AXIS_WIDTH = 1;
+
+    int graph_origin_y_pos = graph_pixels_height / 2;
+    int graph_origin_x_pos = graph_pixels_width / 2;
 
     for (int row_pos = 0; row_pos < graph_pixels_height; row_pos++)
     {
@@ -77,31 +79,28 @@ void generate_hexcodes(int *hexcodes, int raster_row_length, int graph_pixels_wi
 
             // fill background
             hexcodes[hex_index] = 0x00FF00;
-
-            // x,y plaines
-            if (col_pos == PLOT_Y_AXIS_OFFSET)
+            // x,y
+            if (col_pos == graph_origin_y_pos)
             {
                 hexcodes[hex_index] = 0x000000;
             }
-            if (row_pos == graph_pixels_height - PLOT_X_AXIS_OFFSET)
+            if (row_pos == graph_origin_x_pos)
             {
                 hexcodes[hex_index] = 0x000000;
             }
             // ticks on the axis
-            if (
-                row_pos % graph_tick_pixels_interval == 0 && (col_pos == PLOT_Y_AXIS_OFFSET - 1 || (col_pos > PLOT_Y_AXIS_OFFSET && col_pos <= PLOT_Y_AXIS_OFFSET + 3)))
+            if (row_pos % GRAPH_TICK_PIXELS_DEFAULT_INTERVAL == 0 && (col_pos == graph_origin_y_pos - 1 || col_pos == graph_origin_y_pos + 1))
             {
                 hexcodes[hex_index] = 0x000000;
             }
-            if (
-                col_pos % graph_tick_pixels_interval == 0 && (row_pos == (graph_pixels_height - PLOT_X_AXIS_OFFSET + 1) || (row_pos <= graph_pixels_height - PLOT_X_AXIS_OFFSET && row_pos > (graph_pixels_height - PLOT_X_AXIS_OFFSET - 3))))
+            if (col_pos % GRAPH_TICK_PIXELS_DEFAULT_INTERVAL == 0 && (row_pos == graph_origin_x_pos - 1 || row_pos == graph_origin_x_pos + 1))
             {
                 hexcodes[hex_index] = 0x000000;
             }
             // plot points
             for (int i = 0; i < points_length; i++)
             {
-                if (row_pos == graph_pixels_height - points[i].graph_x_pos && col_pos == points[i].graph_y_pos)
+                if (col_pos == points[i].data_x_pos * GRAPH_SIZE_SCALE + graph_origin_x_pos && row_pos == graph_origin_y_pos - points[i].data_y_pos * GRAPH_SIZE_SCALE)
                 {
                     hexcodes[hex_index] = 0x000000;
                 }
@@ -112,13 +111,25 @@ void generate_hexcodes(int *hexcodes, int raster_row_length, int graph_pixels_wi
     }
 }
 
-void calculate_graph_positions(struct Point *points, int points_length, int graph_tick_pixels_interval)
+void calculate_graph_size(struct Point *points, unsigned int points_length, unsigned int *graph_pixels_width, unsigned int *graph_pixels_height)
 {
+    int max_distance = 0;
+    struct Point furthest_point;
+
     for (int i = 0; i < points_length; i++)
     {
-        points[i].graph_x_pos = points[i].data_x_pos * graph_tick_pixels_interval;
-        points[i].graph_y_pos = points[i].data_y_pos * graph_tick_pixels_interval;
+        int distance = sqrt(
+            (double)points[i].data_x_pos * points[i].data_x_pos + (double)points[i].data_y_pos * points[i].data_y_pos);
+
+        if (distance > max_distance)
+        {
+            max_distance = distance;
+            furthest_point = points[i];
+        }
     }
+
+    *graph_pixels_width = abs(furthest_point.data_x_pos * 4 * GRAPH_SIZE_SCALE);
+    *graph_pixels_height = abs(furthest_point.data_y_pos * 4 * GRAPH_SIZE_SCALE);
 }
 
 int main()
@@ -127,39 +138,22 @@ int main()
 
     int colour_background = 0x00FF00;
 
-    struct Point points[5];
-    points[0].value = 1;
-    points[0].data_x_pos = 2;
-    points[0].data_y_pos = 2;
+    struct Point points[6] = {
+        {-2, 2, 0, 0, 1},
+        {3, 3, 0, 0, 1},
+        {-5, -5, 0, 0, 1},
+        {9, -9, 0, 0, 1},
+        {4, 4, 0, 0, 1},
+        {6, 6, 0, 0, 1}
+        };
 
-    points[1].value = 1;
-    points[1].data_x_pos = 3;
-    points[1].data_y_pos = 3;
+    unsigned int graph_pixels_width, graph_pixels_height;
 
-    points[2].value = 1;
-    points[2].data_x_pos = 4;
-    points[2].data_y_pos = 4;
+    unsigned int points_length = sizeof(points) / sizeof(points[0]);
+    calculate_graph_size(points, points_length, &graph_pixels_width, &graph_pixels_height);
 
-    points[3].value = 1;
-    points[3].data_x_pos = 5;
-    points[3].data_y_pos = 5;
-
-    points[4].value = 1;
-    points[4].data_x_pos = 6;
-    points[4].data_y_pos = 6;
-
-
-    unsigned int graph_pixels_width = 100;
-    unsigned int graph_pixels_height = 100;
-    unsigned int graph_tick_pixels_interval = 5;
-
-    // set_graph_dimensions(&graph_pixels_width, &graph_pixels_height, &graph_tick_pixels_interval, points);
-
-    int points_length = sizeof(points) / sizeof(points[0]);
-    calculate_graph_positions(points, points_length, graph_tick_pixels_interval);
-
-    int raster_row_length = (graph_pixels_width * PIXEL_LENGTH) + 1;   // the number of characters representing pixels in a row times 9 digits for the triplet and each of their white spaces
-    int raster_length = (raster_row_length * graph_pixels_height) + 1; // the number of characters in total as a single line this is the length of all the pixels
+    unsigned int raster_row_length = (graph_pixels_width * PIXEL_LENGTH) + 1;          // the number of characters representing pixels in a row times 9 digits for the triplet and each of their white spaces
+    unsigned long raster_length = (long)(raster_row_length * graph_pixels_height + 1); // the number of characters in total as a single line this is the length of all the pixels
 
     char raster[raster_length];
     memset(raster, 0, sizeof(raster));
@@ -167,7 +161,7 @@ int main()
     int hexcodes[raster_length];
     memset(hexcodes, 0, sizeof(hexcodes));
 
-    generate_hexcodes(hexcodes, raster_row_length, graph_pixels_width, graph_pixels_height, graph_tick_pixels_interval, points, points_length);
+    generate_hexcodes(hexcodes, raster_row_length, graph_pixels_width, graph_pixels_height, points, points_length);
 
     paint_raster(raster, raster_row_length, hexcodes, graph_pixels_width, graph_pixels_height);
 
