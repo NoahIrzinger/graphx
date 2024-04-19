77774fd2aa08f3c797d6b4494d764e1e68187c42
Noah Irzinger <noah.irzinger@gmail.com>
2024-04-19 00:36:24 -0700

    update to use Bresenham algorithm for line drawing

--- example.c
+++ example.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "graphx.h"
 
+#define ARRAY_SIZE 10
+
 int main()
 {
-    unsigned int x_values[] = {1, 5, 2, 3, 4};
-    unsigned int y_values[] = {1, 5, 2, 3, 4};
-    unsigned int values[] = {1, 2, 3, 4, 5};
+    // unsigned int x_values[] = {1, 5, 2, 3, 4};
+    // unsigned int y_values[] = {1, 5, 2, 3, 4};
+    // unsigned int values[] = {1, 2, 3, 4, 5};
+
+    int x_values[ARRAY_SIZE];
+    int y_values[ARRAY_SIZE];
+    int values[ARRAY_SIZE];
+
+    int max = 100;
+    int min = -100;
+
+    srand(time(NULL)); // Seed the random number generator
+
+    // Generate positive and negative random values for x_values, y_values, and values arrays
+    for (int i = 0; i < ARRAY_SIZE; i++) {
+        x_values[i] = (rand() % (max - min + 1)) + min; // Generates random integers between -100 and 100
+        y_values[i] = (rand() % (max - min + 1)) + min; // Generates random integers between -100 and 100
+        values[i] = (rand() % (max - min + 1)) + min;   // Generates random integers between -100 and 100
+    }
 
     unsigned int x_values_size = sizeof(x_values) / sizeof(x_values[0]);
     unsigned int y_values_size = sizeof(y_values) / sizeof(y_values[0]);
-    unsigned int values_size = sizeof(values) / sizeof(values[0]);
+    unsigned int values_size =  sizeof(values) / sizeof(values[0]);
 
     plot_points(x_values, x_values_size, y_values, y_values_size, values, values_size);
 
--- graphx.c
+++ graphx.c
@@ -11,16 +11,16 @@
 #define GRAPH_POINTS_COLOUR 0x000000
 #define OUTPUT_FILE_PATH "output/plot_points.ppm"
 #define PIXEL_LENGTH 12
-#define GRAPH_SIZE_SCALE 20
-#define GRAPH_POINT_RADIUS_SIZE_PIXELS 2
+#define GRAPH_SIZE_SCALE 3
+#define GRAPH_POINT_RADIUS_SIZE_PIXELS 1
 
 struct Point
 {
     int data_x_pos;
     int data_y_pos;
-    unsigned int graph_x_pos;
-    unsigned int graph_y_pos;
-    size_t value;
+    int graph_x_pos;
+    int graph_y_pos;
+    int value;
 };
 
 void write_raster_to_ppm(char raster_data[], char output_file_path[], unsigned int graph_pixels_width, unsigned int graph_pixels_height)
@@ -66,7 +66,61 @@ void paint_raster(char *raster, unsigned int raster_row_length, int hex_values[]
     strcat(raster, "\0");
 }
 
-void generate_hexcodes(int *hexcodes, unsigned int raster_row_length, unsigned int graph_pixels_width, unsigned int graph_pixels_height, struct Point points[], unsigned int points_length)
+void compute_bresenham_points(struct Point *points, unsigned int points_length, struct Point *bresenham_points, unsigned int *bresenham_points_length, unsigned int graph_pixels_width, unsigned int graph_pixels_height)
+{
+    int graph_origin_y_pos = graph_pixels_height / 2;
+    int graph_origin_x_pos = graph_pixels_width / 2;
+
+    unsigned int j = 0;
+
+    for (unsigned int i = 0; i < points_length - 1; i++)
+    {
+        struct Point cur_point = points[i];
+        struct Point next_point = points[i + 1];
+
+        int cur_graph_x_pos = cur_point.data_x_pos * GRAPH_SIZE_SCALE + graph_origin_x_pos;
+        int cur_graph_y_pos = graph_origin_y_pos - cur_point.data_y_pos * GRAPH_SIZE_SCALE;
+
+        int next_graph_x_pos = next_point.data_x_pos * GRAPH_SIZE_SCALE + graph_origin_x_pos;
+        int next_graph_y_pos = graph_origin_y_pos - next_point.data_y_pos * GRAPH_SIZE_SCALE;
+
+        // compute line between these point's graph positions
+        int dx = abs(next_graph_x_pos - cur_graph_x_pos);
+        int dy = -abs(next_graph_y_pos - cur_graph_y_pos);
+
+        int sx = next_graph_x_pos > cur_graph_x_pos ? 1 : -1;
+        int sy = next_graph_y_pos > cur_graph_y_pos ? 1 : -1;
+
+        int err = dx + dy;
+        int e2;
+
+        while (1)
+        {
+            struct Point p = {0, 0, cur_graph_x_pos, cur_graph_y_pos, 0};
+            bresenham_points[j] = p;
+
+            if (cur_graph_x_pos == next_graph_x_pos && cur_graph_y_pos == next_graph_y_pos)
+                break;
+
+            e2 = 2 * err;
+            if (e2 >= dy)
+            {
+                err += dy;
+                cur_graph_x_pos += sx;
+            }
+            if (e2 <= dx)
+            {
+                err += dx;
+                cur_graph_y_pos += sy;
+            }
+            j++;
+        }
+    }
+    // shorten bresenham_points_length
+    *bresenham_points_length = j;
+}
+
+void generate_hexcodes(int *hexcodes, unsigned int raster_row_length, unsigned int graph_pixels_width, unsigned int graph_pixels_height, struct Point points[], unsigned int points_length, struct Point *bresenham_points, unsigned int *bresenham_points_length)
 {
     int graph_origin_y_pos = graph_pixels_height / 2;
     int graph_origin_x_pos = graph_pixels_width / 2;
@@ -112,35 +166,13 @@ void generate_hexcodes(int *hexcodes, unsigned int raster_row_length, unsigned i
                 {
                     hexcodes[hex_index] = GRAPH_POINTS_COLOUR;
                 }
-                // draw line graph
-                if (i < points_length - 1)
+            }
+            // use Bresenham's algorithm to draw line between points
+            for (int i = 0; i < *bresenham_points_length; i++)
+            {
+                if (col_pos == bresenham_points[i].graph_x_pos && row_pos == bresenham_points[i].graph_y_pos)
                 {
-                    int next_graph_x_pos = points[i + 1].data_x_pos * GRAPH_SIZE_SCALE + graph_origin_x_pos;
-                    int next_graph_y_pos = graph_origin_y_pos - points[i + 1].data_y_pos * GRAPH_SIZE_SCALE;
-
-                    if (col_pos > graph_x_pos && col_pos < next_graph_x_pos)
-                    {
-                        double y_diff = next_graph_y_pos - graph_y_pos;
-                        double x_diff = next_graph_x_pos - graph_x_pos;
-
-                        if (x_diff != 0)
-                        {
-                            double points_slope = y_diff / x_diff;
-
-                            double cur_y_diff = next_graph_y_pos - row_pos;
-                            double cur_x_diff = next_graph_x_pos - col_pos;
-
-                            if (cur_y_diff != 0)
-                            {
-                                double cur_slope = cur_y_diff / cur_x_diff;
-
-                                if (cur_slope == points_slope)
-                                {
-                                    hexcodes[hex_index] = GRAPH_POINTS_COLOUR;
-                                }
-                            }
-                        }
-                    }
+                    hexcodes[hex_index] = GRAPH_POINTS_COLOUR;
                 }
             }
         }
@@ -168,7 +200,7 @@ void calculate_graph_size(struct Point *points, unsigned int points_length, unsi
     *graph_pixels_height = abs(furthest_point.data_y_pos * 4 * GRAPH_SIZE_SCALE);
 }
 
-int validate_input_data(unsigned int x_values[], unsigned int x_values_size, unsigned int y_values[], unsigned int y_values_size, unsigned int values[], unsigned int values_size)
+int validate_input_data(int x_values[], unsigned int x_values_size, int y_values[], unsigned int y_values_size, int values[], unsigned int values_size)
 {
     if (x_values_size != y_values_size || x_values_size != values_size || y_values_size != values_size)
     {
@@ -180,12 +212,20 @@ int validate_input_data(unsigned int x_values[], unsigned int x_values_size, uns
 
 int compare_points(const void *point_a, const void *point_b)
 {
-    return (((struct Point *)point_a)->data_x_pos - ((struct Point *)point_b)->data_y_pos);
+    if (((struct Point *)point_a)->data_x_pos < ((struct Point *)point_b)->data_x_pos)
+    {
+        return -1;
+    }
+    if (((struct Point *)point_a)->data_x_pos > ((struct Point *)point_b)->data_x_pos)
+    {
+        return 1;
+    }
+    return 0;
 }
 
-void generate_points(struct Point *points, unsigned int number_of_points, unsigned int x_values[], unsigned int y_values[], unsigned int values[])
+void generate_points(struct Point *points, unsigned int number_of_points, int x_values[], int y_values[], int values[])
 {
-    for (int i = 0; i < number_of_points; i++)
+    for (unsigned int i = 0; i < number_of_points; i++)
     {
         struct Point p = {x_values[i], y_values[i], 0, 0, values[i]};
         points[i] = p;
@@ -193,31 +233,44 @@ void generate_points(struct Point *points, unsigned int number_of_points, unsign
     qsort(points, number_of_points, sizeof(struct Point), compare_points);
 }
 
-int plot_points(unsigned int x_values[], unsigned int x_values_size, unsigned int y_values[], unsigned int y_values_size, unsigned int values[], unsigned int values_size)
+int plot_points(int x_values[], unsigned int x_values_size, int y_values[], unsigned int y_values_size, int values[], unsigned int values_size)
 {
     int err = validate_input_data(x_values, x_values_size, y_values, y_values_size, values, values_size);
     if (err != 0)
     {
         return err;
     }
+    printf("input data is valid\n");
 
     struct Point points[values_size];
     generate_points(points, values_size, x_values, y_values, values);
+    printf("generated points\n");
 
     unsigned int graph_pixels_width, graph_pixels_height;
     calculate_graph_size(points, values_size, &graph_pixels_width, &graph_pixels_height);
+    printf("calculated graph size\n");
 
     unsigned int raster_row_length = graph_pixels_width * PIXEL_LENGTH + 1;    // the number of characters representing pixels in a row times 9 digits for the triplet and each of their white spaces
     unsigned long raster_length = raster_row_length * graph_pixels_height + 1; // the number of characters in total as a single line this is the length of all the pixels
 
     char *raster = (char *)malloc(raster_length * sizeof(char));
     int *hexcodes = (int *)malloc(raster_length * sizeof(int));
+    unsigned int bresenham_points_length = raster_length; // at first set equal to the raster length and realloc when we know the number of pixel to draw lines
 
-    generate_hexcodes(hexcodes, raster_row_length, graph_pixels_width, graph_pixels_height, points, values_size);
+    struct Point *bresenham_points = (struct Point *)malloc(raster_length * sizeof(struct Point)); // estimate the max size by the longer size of the length or width multiplied by the number of points
+
+    // unsigned int bresenham_points_length = (unsigned int)sizeof(bresenham_points)/sizeof(bresenham_points[0]);
+    compute_bresenham_points(points, values_size, bresenham_points, &bresenham_points_length, graph_pixels_width, graph_pixels_height);
+    printf("computed points using bresenham algorithm\n");
+
+    generate_hexcodes(hexcodes, raster_row_length, graph_pixels_width, graph_pixels_height, points, values_size, bresenham_points, &bresenham_points_length);
+    printf("generated hex codes\n");
 
     paint_raster(raster, raster_row_length, hexcodes, graph_pixels_width, graph_pixels_height);
+    printf("painted raster\n");
 
     write_raster_to_ppm(raster, OUTPUT_FILE_PATH, graph_pixels_width, graph_pixels_height);
+    printf("raster data written to file\n");
 
     free(raster);
     free(hexcodes);
