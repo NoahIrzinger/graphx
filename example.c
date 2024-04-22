3bc1b81e745fc3138d200b346ae45b34e20e3b61
Noah Irzinger <noah.irzinger@gmail.com>
2024-04-22 00:02:28 -0700

    use c99 compatibility

--- example.c
+++ example.c
@@ -22,7 +22,8 @@ int main()
     srand(time(NULL)); // Seed the random number generator
 
     // Generate positive and negative random values for x_values, y_values, and values arrays
-    for (int i = 0; i < ARRAY_SIZE; i++) {
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
         x_values[i] = (rand() % (max - min + 1)) + min; // Generates random integers between -100 and 100
         y_values[i] = (rand() % (max - min + 1)) + min; // Generates random integers between -100 and 100
         values[i] = (rand() % (max - min + 1)) + min;   // Generates random integers between -100 and 100
@@ -30,7 +31,7 @@ int main()
 
     unsigned int x_values_size = sizeof(x_values) / sizeof(x_values[0]);
     unsigned int y_values_size = sizeof(y_values) / sizeof(y_values[0]);
-    unsigned int values_size =  sizeof(values) / sizeof(values[0]);
+    unsigned int values_size = sizeof(values) / sizeof(values[0]);
 
     points_plot(x_values, y_values, values, values_size);
 
